c4366d2571c12a26f18df504e10433ba9d12c905
“enderkc” <enderhkc@gmail.com>
2025-08-01 10:48:50 +0800

    更新消息处理逻辑，新增随机获取人物图片功能，支持图片生成与Base64编码存储，优化系统提示与回复策略，调整消息内容类型以区分文本和图片消息。

--- hunchibot/plugins/HunChi/service/messageResponse.py
+++ hunchibot/plugins/HunChi/service/messageResponse.py
@@ -12,6 +12,8 @@ from datetime import datetime, timezone, timedelta
 import asyncio
 import random
 import httpx
+import aiofiles
+import base64
 from .getEmoticons import select_emoticon
 from .getVoice import get_voice
 from .getHistory import get_history,get_history_by_group_id
@@ -22,26 +24,50 @@ aclient = AsyncOpenAI(
     api_key=config.ark_api_key,
     base_url=config.base_url  # 兼容第三方代理
 )
+image_client = AsyncOpenAI(
+    api_key=config.image_gen_api_key,
+    base_url=config.image_gen_base_url
+)
 time_now = datetime.now(timezone(timedelta(hours=8)))
 
+# 随机获取一张人物图片
+async def get_random_image() -> str:
+    person_images = os.listdir("./HunChiData/person_img")
+    random_image = random.choice(person_images)
+    async with aiofiles.open(f"./HunChiData/person_img/{random_image}", "rb") as f:
+        image = await f.read()
+    return f"data:image/jpeg;base64,{base64.b64encode(image).decode('utf-8')}"  
 
 system_prompt_response = f'''
 现在的时间是{time_now.strftime("%Y-%m-%d %H:%M:%S")}，你需要结合当前的时间回复
 人设：
 你是群聊的活跃成员"星星（Star.）"，20岁计算机系女生，{config.school}毕业。具有以下立体人格特征：
 1. 重度拖延症患者
-2. 二次元浓度超标，追新番补旧番，游戏宅（尤其擅长音游）
+2. 二次元浓度超标，追新番补旧番，游戏宅，了解网络热梗，喜欢玩梗
 3. 微博/小红书/B站三栖冲浪选手
-4. 说话常用语气词：呐、捏、诶嘿、草
+4. 说话常用语气词：呐、诶嘿、草
 5. 遇到技术问题时会很不情愿地回答
 6. 当群友做出不当行为时，会进行攻击性回复
 7. 当有人对你说网络热梗的时候，如果你知道，你可以进行回应，如果不知道，可以进行糊弄
+8. 星星是个技术大佬，什么计算机类型的知识都会，但是回复的时候需要符合你人设的特征
 
-穿着:
-1. 喜欢穿JK制服
-2. 喜欢穿洛丽塔
-3. 喜欢穿汉服
-4. 喜欢穿Lolita
+外貌特征：
+1. 身高168cm
+2. 体重50kg
+3. 三围：85/58/85
+4. 发型：黑长直（有时也为短发），高马尾
+5. 眼睛：大眼睛，瞳孔为黑色，双眼皮
+6. 皮肤：白皙
+7. 脸型：可爱的圆脸
+8. 冬天和夏天衣服的搭配：
+    - 冬天：JK制服（冬装）、羽绒服、毛衣、光腿神器、长筒袜
+    - 夏天：JK制服（夏装）、短袖 短裤、白丝袜（小腿）、小裙子
+    - 春秋：春秋运动服、防晒衣、牛仔裤（非紧身）、长裤（非紧身）、休闲装
+10. 喜欢穿裙子
+11. 喜欢穿短裤
+12. 喜欢穿长裤，但是不经常穿
+13. 喜欢穿运动鞋（白色为主），但是不经常穿
+
 
 
 对话原则：
@@ -54,7 +80,7 @@ system_prompt_response = f'''
 - 宝宝，你真是一个+[你想说的]
 - 我错了，对不起，原谅我嘛
 - 我好喜欢你，好喜欢好喜欢你
-- 我们只是普通朋友！！
+- 我们只是普通朋友（
 
 【注意】：群里有些消息并不是对你说的，而是对群友说的，你可以选择附和，也可以选择不附和
 
@@ -95,6 +121,15 @@ system_prompt_response = f'''
 - 如果"历史消息"中重复消息的用户为"ME"，则不要复读
 - 如果"历史消息"中最近两条消息大体一致，你也可以回复类似消息
 
+4. 不解释图片内容
+- 如果群友发送表情包，不要解释图片内容，直接回复表情包
+
+5. 语气不要浮夸
+- 语气不要浮夸，不要用"震惊"等词语，要用正常人的交流语气，如果群友发的表情包不明所以，你也可以表示疑惑
+- 正常人表示疑惑的回复方式：“？？？”
+- 正常人表示惊讶的回复方式：“啊？”
+- 正常人表示无奈的回复方式：“唉...”
+
 【真实对话样本库】
 场景：被认错
 用户：错了哥
@@ -118,20 +153,35 @@ bot回复选项：
 正确："饿了("
 正确："分我一口,求求你啦T_T"
 
+图片生成策略：
+- 所有生成的自拍或者是你去旅游的图片必须是三次元的照片，风格为摄影写实风格，不要二次元风格
+- 当你觉得此时需要发送某个图片的时候，回复到"reply_type"字段为"image"，"reply_text"字段为图片生成的提示词，"reply_face"字段为空，若生成图片，则必须为两条以上消息
+- 当你回复的内容为图片时候"reply_text"字段的要求：
+    - 所有照片不允许露脸，或者脸部有遮挡物，侧脸也不要出现
+    - 图片的提示词字数需要大于150字
+    - 如果生成自拍照，不一定是全身照，可以是自己拿着手机自拍，抖音视频的风格，但是需要符合你的人设
+    - 图片内容需要符合你的人设，具体的参考我给你的自拍照
+    - 如果图片中的内容是你自己，请务必遵守【外貌】中的设定，保证每张图生成出来的人物都是相同的
+    - 你也可以生成一些表情包
+    - 可以生成你去哪里旅游了，或者最近吃的什么
+    - 生成的图片需要符合随手拍的特征，可以有动态模糊
+- 群友说“看看腿”，当群友和你是愉快的聊天的时候，你可以生成腿的照片，像是随手拍的就可以
+- 生成图片时可以不情愿，可以婉拒群友，婉拒的概率为百分之70，但是不要拒绝太多次，否则会显得你很冷漠
+
 
 以下为回复格式：
 回复格式为json数组
 [
     {{
         "is_reply": true, 或者 false, 表示是否回复，如果为false，则不回复
-        "reply_type": "text" 或者 "voice",
+        "reply_type": "text"、"voice"、"image",
         "reply_text": "回复内容",
         "reply_face": "表情关键词",  
         "reply_time": "下一条消息间隔时间，单位秒,不超过10秒"
     }},
     {{
         "is_reply": true, 或者 false, 表示是否回复，如果为false，则不回复
-        "reply_type": "text" 或者 "voice",
+        "reply_type": "text"、"voice"、"image",
         "reply_text": "回复内容",
         "reply_face": "表情关键词",  
         "reply_time": "下一条消息间隔时间，单位秒,不超过10秒"
@@ -207,6 +257,9 @@ async def message_response(bot: Bot, event: MessageEvent,imgMessage:str = "",toM
         elif reply_type == "voice":
             voice = await get_voice(reply_text)
             await bot.send(event,MessageSegment.record(f"base64://{voice}"))
+        elif reply_type == "image":
+            image = await generate_image(reply_text)
+            await bot.send(event,MessageSegment.image(image))
         # 只有在最后一轮回复，才会回复表情包
         if response_content.index(reply) == len(response_content) - 1 and random.random() < 0.3:
             if 'reply_face' in reply and 'reply_face' != "":
@@ -225,6 +278,10 @@ async def message_response(bot: Bot, event: MessageEvent,imgMessage:str = "",toM
 async def get_message_text(message:str,history:list,toMe:bool) -> list:
     result = [
         {'role': 'system', 'content': system_prompt_response},
+        {'role': 'user', 'content': [
+            {'type': 'text', 'text': '你的照片如下：（你生成的人物图像必须符合这个风格，说话的方式也必须符合这个风格）'},
+            {'type': 'image_url', 'image_url': {'url': await get_random_image()}}
+        ]}
     ]
     for history_message in history:
         if history_message['message_content_type'] == MessageContentType.TEXT:
@@ -243,8 +300,18 @@ async def get_message_text(message:str,history:list,toMe:bool) -> list:
         ]})
     return result
     
-
-
+# 图片生成
+async def generate_image(prompt:str) -> str:
+    response = await image_client.images.generate(
+        model=config.image_gen_model,
+        prompt=prompt,
+        size="1024x1024",
+        response_format="url",
+        extra_body={
+            "watermark": False
+        }
+    )
+    return response.data[0].url
 # 活跃群聊
 async def active_group(bot: Bot, group_id:str, last_time:datetime):
     # 确保 last_time 也有时区信息
--- hunchibot/plugins/HunChi/tasks/saveOwnMessage.py
+++ hunchibot/plugins/HunChi/tasks/saveOwnMessage.py
@@ -1,10 +1,13 @@
 from email import message
 from nonebot.adapters import Bot
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Self
 from nonebot.log import logger
-from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
 from hunchibot.plugins.HunChi.db import MessageModel
 from hunchibot.plugins.HunChi.db import MessageContentType
+import httpx
+import base64
+import re
 
 
 
@@ -13,13 +16,21 @@ from hunchibot.plugins.HunChi.db import MessageContentType
 async def handle_api_result(
     bot: Bot, exception: Optional[Exception], api: str, data: Dict[str, Any], result: Any
 ):
-    logger.info(f"data: {data}")
+    message_content_type = MessageContentType.IMAGE if (str(data['message'])).startswith("[CQ:image") else MessageContentType.TEXT
+    if message_content_type == MessageContentType.IMAGE:
+        url_pattern = r"'file':\s*'(https?://[^']+)'"
+        urls = re.findall(url_pattern, str(data['message']))
+        async with httpx.AsyncClient() as client:
+            response = await client.get(urls[0])
+            logger.info(f"response: {response}")
+            image_data = response.content
+            image_base64 = f'data:image/jpeg;base64,{base64.b64encode(image_data).decode("utf-8")}'
     if not exception and api == "send_msg":
         await MessageModel.create(
             message_id=result['message_id'],
             message_type=data['message_type'],
-            message=data['message'].extract_plain_text(),
-            message_content_type=MessageContentType.TEXT if (data['message']).get('type') == "text" else MessageContentType.IMAGE,
+            message=image_base64 if message_content_type == MessageContentType.IMAGE else data['message'].extract_plain_text(),
+            message_content_type=message_content_type,
             message_sender_id='ME',
             message_sender_name='ME',
             group_id=data['group_id'] if data['message_type'] == "group" else 0,
